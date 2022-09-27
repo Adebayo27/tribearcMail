f11516222e06d7d6951ff18bea50d80beacb1834
Adekoya Adebayo <adekoya.adebayojubril@gmail.com>
2022-09-27 16:46:29 +0100

    add sms wrapper

--- src/README.md
+++ src/README.md
@@ -10,14 +10,13 @@ Require `adebayo27/tribearcmail` using composer.
 
 Add the following to the .env file
 
-`
-TRIBEARC_MAIL_API_KEY="Your api key gotten from tribearc mail"
-TRIBEARC_MAIL_FROM_NAME="Your from name"
-TRIBEARC_MAIL_FROM_EMAIL="your from email"
-TRIBEARC_MAIL_REPLY_TO="your reply to email"
-TRIBEARC_MAIL_BUSINESS_ADDRESS="your business address (registered with tribe)"
-TRIBEARC_MAIL_BUSINESS_NAME="your business name (registered with tribe)"
-`
+`TRIBEARC_MAIL_API_KEY="Your api key gotten from tribearc mail"`<br/>
+`TRIBEARC_MAIL_FROM_NAME="Your from name"`<br/>
+`TRIBEARC_MAIL_FROM_EMAIL="your from email"`<br/>
+`TRIBEARC_MAIL_REPLY_TO="your reply to email"`<br/>
+`TRIBEARC_MAIL_BUSINESS_ADDRESS="your business address (registered with tribe)"`<br/>
+`TRIBEARC_MAIL_BUSINESS_NAME="your business name (registered with tribe)"`<br/>
+
 
 ### Example: using the library
 
@@ -32,10 +31,54 @@ $tribearc = new TribearcMail();
 $response = $tribearc->tribearcSendMail('Subject', 'Email body', 'email address');
 
 //send bulk email
-$responseBulk = $tribearc->tribearcSendBulkMail('Subject', 'Email body', 'ade@mail.com,bayo@mail.com,koya@mail');
+$response = $tribearc->tribearcSendBulkMail('Subject', 'Email body', 'ade@mail.com,bayo@mail.com,koya@mail');
 
 
 //send instant sms 
-coming soon
+$response = $tribearc->tribearcSMSInstant(
+    [
+        "from" => "Kent",
+        "to" => "+2348012345678",
+        "message" => "Hello Adebayo",
+    ]
+);
+
+//check sms balance
+$response = $tribearc->tribearcCheckSMSBalance();
+
+//check from email status
+$response = $tribearc->tribearcCheckFromEmailStatus('hello@adebayo.com');
+
+//send email to saved list
+$response = $tribearc->tribearcSendEmailToSavedList(
+    [        
+        'title' => 'Campaign',
+        'subject' => 'subject',
+        'html_text' => 'Hello',
+        'plain_text' => '', //optional
+        'list_ids' => 'your list ids', //Required only if you set send_campaign to 1 and no segment_ids are passed in. List IDs should be single or comma-separated. The encrypted & hashed ids can be found under View all lists section named ID.
+        'exclude_list_ids' => '', //Lists to exclude from your campaign. List IDs should be single or comma-separated. The encrypted & hashed ids can be found under View all lists section named ID. (optional)
+        'track_opens' => '1', //Set to 0 to disable, 1 to enable and 2 for anonymous opens tracking.
+        'query_string' => '', //eg. Google Analytics tags (optional)
+        'brand_id' => '', //Required only if you are creating a 'Draft' campaign (send_campaign set to 0 or left as default). Brand IDs can be found under 'Brands' page named ID
+        'track_clicks' => '1', //Set to 0 to disable, 1 to enable and 2 for anonymous opens tracking.
+        'send_campaign' => '1', //Set to 1 if you want to send the campaign as well and not just create a draft. Default is 0.
+        'segment_ids' => '', //Required only if you set send_campaign to 1 and no list_ids are passed in. Segment IDs should be single or comma-separated. Segment ids can be found in the segments setup page.
+        'schedule_date_time' => '', //Campaign will be scheduled if a valid date/time is passed. Date/time format eg. June 15, 2021 6:05pm. The minutes part of the time has to be in increments of 5, eg. 6pm, 6:05pm, 6:10pm, 6:15pm.
+        'schedule_timezone' => '', //Eg. 'America/New_York'. See the list of PHP's supported timezones. This parameter only applies if you're scheduling your campaign with schedule_date_time parameter. TribeArc will use your default timezone if this parameter is empty.
+    ]
+
+);
+
+//subscribe to list
+$response = $tribearc->tribearcSubscribeToList(
+    [
+        "name" => "Kent",
+        "email" => "hello@adebayo.com",
+        "list" => "your list id",
+        "country" => "NG",
+    ]
+);
+
 
 ```
--- src/TribearcMail.php
+++ src/TribearcMail.php
@@ -56,6 +56,7 @@ class TribearcMail {
         curl_close($curl);
         return $res;
     }
+    
 
     static function tribearcSendBulkMail($subject, $content, $mails){
         $curl = curl_init();
@@ -81,6 +82,109 @@ class TribearcMail {
             'business_name' => env('TRIBEARC_MAIL_BUSINESS_NAME'),
         ));
         curl_setopt($curl, CURLOPT_HTTPHEADER, array('Api-Token: '. env('TRIBEARC_MAIL_API_KEY')));
+
+        $response = curl_exec($curl);
+        $res = $response;
+        curl_close($curl);
+        return $res;
+    }
+
+    //send campaign
+    static function tribearcSendEmailToSavedList($data){
+        $allData = array_merge([ 
+            "api_key" => env('TRIBEARC_MAIL_API_KEY'),
+            'from_name' => env('TRIBEARC_MAIL_FROM_NAME'),
+            'from_email' => env('TRIBEARC_MAIL_FROM_EMAIL'),
+            'reply_to' => env('TRIBEARC_MAIL_REPLY_TO'),
+        ], $data);
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, 'https://newsletter.tribearc.com/api/campaigns/create.php');
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); //
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false); //
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST'); //
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $allData);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Api-Token: '. env('TRIBEARC_MAIL_API_KEY')));
+
+        $response = curl_exec($curl);
+        $res = $response;
+        curl_close($curl);
+        return $res;
+    }
+
+    //subscribe to list
+    static function tribearcSubscribeToList($data){
+        $allData = array_merge([ "api_key" => env('TRIBEARC_MAIL_API_KEY'), "hp" => "honeypot"], $data);
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, 'https://newsletter.tribearc.com/subscribe');
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); //
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false); //
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST'); //
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $allData);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Api-Token:' . env('TRIBEARC_MAIL_API_KEY')));
+
+        $response = curl_exec($curl);
+        $res = $response;
+        curl_close($curl);
+        return $res;
+    }
+
+     //check from-email status
+     static function tribearcCheckFromEmailStatus($from_email){
+        $allData = [ 
+            "api_key" => env('TRIBEARC_MAIL_API_KEY'),
+            "from_email" => $from_email
+        ];
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, 'https://newsletter.tribearc.com/api/identity/status.php');
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); //
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false); //
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST'); //
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $allData);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Api-Token:' . env('TRIBEARC_MAIL_API_KEY')));
+
+        $response = curl_exec($curl);
+        $res = $response;
+        curl_close($curl);
+        return $res;
+    }
+
+    //send sms
+    static function tribearcSMSInstant($data){
+        $allData = array_merge([ "api_key" => env('TRIBEARC_MAIL_API_KEY')], $data);
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, 'https://newsletter.tribearc.com/api/sms/send_now.php');
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); //
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false); //
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST'); //
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $allData);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Api-Token:' . env('TRIBEARC_MAIL_API_KEY')));
+
+        $response = curl_exec($curl);
+        $res = $response;
+        curl_close($curl);
+        return $res;
+    }
+
+    //check sms balance
+    static function tribearcCheckSMSBalance(){
+        $allData = [ "api_key" => env('TRIBEARC_MAIL_API_KEY')];
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, 'https://newsletter.tribearc.com/api/sms/get_balance.php');
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); //
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false); //
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST'); //
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $allData);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Api-Token:' . env('TRIBEARC_MAIL_API_KEY')));
 
         $response = curl_exec($curl);
         $res = $response;
